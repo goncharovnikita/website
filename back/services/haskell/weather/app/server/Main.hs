5172883da1c4a7da343d2e71762b2426914cf919
Goncharov Nikita <goncharovnikita.dev@gmail.com>
2020-01-24 08:46:14 +0300

    Add cabal update
    Put localhost into env variable

--- back/services/haskell/weather/app/server/Main.hs
+++ back/services/haskell/weather/app/server/Main.hs
@@ -2,11 +2,23 @@ module Main where
 
 import Database.MongoDB
 import Control.Monad.Trans (liftIO)
+import System.Environment (lookupEnv)
 
 import Server (start)
 
 main :: IO ()
 main = do
-    pipe <- connect (host "localhost")
+    dbHost <- getDbHost
+    pipe <- connect (host dbHost)
     start pipe
     close pipe
+
+getDbHost :: IO (String)
+getDbHost = do
+    let dbHostEnvName = "DB_HOST"
+    mbStr <- lookupEnv dbHostEnvName
+    case mbStr of 
+      Just str -> return str
+      Nothing -> do
+          putStrLn $ mconcat [dbHostEnvName, " env variable does not exists, falling back to localhost"]
+          return "localhost"
