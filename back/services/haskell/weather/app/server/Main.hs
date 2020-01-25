21789a25fe19b6d832829573921574fecfe5f1cb
Goncharov Nikita <goncharovnikita.dev@gmail.com>
2020-01-25 17:52:26 +0300

    Setup docker build

--- back/services/haskell/weather/app/server/Main.hs
+++ back/services/haskell/weather/app/server/Main.hs
@@ -2,12 +2,14 @@ module Main where
 
 import Database.MongoDB
 import Control.Monad.Trans (liftIO)
+import System.IO (hSetBuffering, stdout, BufferMode(NoBuffering))
 
 import Server (start)
 import Config
 
 main :: IO ()
 main = do
+    hSetBuffering stdout NoBuffering
     dbHost <- getDbHost
     pipe <- connect (host dbHost)
     start pipe
