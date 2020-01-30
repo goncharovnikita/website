78c2135041a9652ee4da37858dc16364f8f67414
Goncharov Nikita <goncharovnikita.dev@gmail.com>
2020-01-30 08:44:18 +0300

    add beta

---
+++ front2/layouts/default.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <nuxt />
+  </div>
+</template>
+
+<style>
+html {
+  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+    Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+
+*,
+*:before,
+*:after {
+  box-sizing: border-box;
+  margin: 0;
+}
+
+@font-face {
+  src: url('/fonts/Merriweather-Regular.ttf');
+  font-family: 'Merriweather-Regular';
+}
+
+@font-face {
+  src: url('/fonts/Quicksand-Regular.ttf');
+  font-family: 'Quicksand-Regular';
+}
+
+* {
+  --primary-color: #e6f3ff;
+  --sidebar-color: #e6f3ffe8;
+  --accent-color: #66b5ff;
+  --deep-blue: #004080;
+  --typeface-color: #a3c2c2;
+
+  --titles-font: 'Merriweather-Regular';
+  --typeface-font: 'Quicksand-Regular';
+}
+
+* {
+  margin: 0;
+  padding: 0;
+  font-family: var(--typeface-font);
+}
+</style>
---
+++ front2/pages/index.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="layout">
+    <div class="sidebar">
+      <div class="sidebar-avatar">
+        <img src="images/avatar.jpg" alt="" class="avatar" />
+      </div>
+      <div class="credentials">
+        <h3>Goncharov Nikita</h3>
+      </div>
+      <div class="sidebar-footer">
+        <div class="flex">
+          <a href="https://github.com/goncharovnikita"
+            ><img class="icon-img" src="icons/github.svg" alt=""
+          /></a>
+          <a href="https://medium.com/@cashalot"
+            ><img class="icon-img" src="icons/medium.svg" alt=""
+          /></a>
+          <a href="https://twitter.com/jamberspof"
+            ><img class="icon-img" src="icons/twitter.svg" alt=""
+          /></a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {}
+}
+</script>
+
+<style>
+.layout {
+  opacity: 1;
+  height: 100vh;
+  display: grid;
+  grid-template-areas: 'sidebar content';
+  grid-template-rows: 100% 100%;
+  z-index: 199;
+
+  @media (min-width: 600px) {
+    display: grid;
+    grid-template-areas: 'sidebar content';
+    grid-template-columns: 33% 67%;
+    z-index: 199;
+    background-size: cover;
+  }
+}
+.sidebar {
+  height: 100%;
+  grid-area: sidebar;
+  background-color: var(--primary-color);
+  display: grid;
+  grid-template-rows: 25% 5% 1fr 7%;
+  grid-template-areas: 'avatar' 'credentials' 'info' 'side-footer';
+  grid-gap: 10px;
+  overflow-y: hidden;
+  opacity: 0.8;
+}
+.sidebar-avatar {
+  grid-area: avatar;
+  min-width: 0;
+  display: grid;
+  grid-template-columns: 25% 50% 25%;
+  grid-template-areas: '. avatar-photo .';
+  padding: 12px;
+}
+.avatar {
+  grid-area: avatar-photo;
+  width: initial;
+  height: 100%;
+  align-self: center;
+  border-radius: 50%;
+  justify-self: center;
+}
+.credentials {
+  grid-area: credentials;
+  font-family: var(--titles-font);
+  align-self: center;
+  justify-self: center;
+}
+.sidebar-footer {
+  grid-area: side-footer;
+  width: 50%;
+  justify-self: center;
+}
+.flex {
+  display: flex;
+  justify-content: space-between;
+}
+.icon-img {
+  height: 5vh;
+  width: auto;
+}
+</style>
