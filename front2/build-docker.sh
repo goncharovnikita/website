2b4a949c26a24cd02e288b3dfdf1c4d0a8894731
Goncharov Nikita <goncharovnikita.dev@gmail.com>
2020-04-07 07:48:32 +0300

    Move front2 -> front

--- front2/build-docker.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-TAG=goncharovnikita/website:$TRAVIS_BRANCH
-docker build --rm -f Dockerfile -t $TAG . && \
-docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD" && \
-docker push $TAG
