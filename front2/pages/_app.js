a3f93b3064ce2cbf70ccccd41ddae2b7ebd8639e
Goncharov Nikita <goncharovnikita.dev@gmail.com>
2020-04-07 07:33:15 +0300

    Connect yandex metrica

--- front2/pages/_app.js
+++ front2/pages/_app.js
@@ -1,11 +1,18 @@
+import { YMInitializer } from 'react-yandex-metrika';
+
 import styles from "./app.module.scss";
 
 import '../styles/index.scss';
 
+const accounts = [57556537];
+
 export default function MyApp({Component, pageProps}) {
   return (
-    <div className={styles.content}>
-      <Component {...pageProps} />
-    </div>
+      <>
+          <YMInitializer accounts={accounts} />
+          <div className={styles.content}>
+              <Component {...pageProps} />
+          </div>
+      </>
   );
 }
