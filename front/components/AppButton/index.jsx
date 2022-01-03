b156a0df74cf714a5316e996f74d3014e85bb64f
Nikita Goncharov <goncharovnikita.dev@gmail.com>
2022-01-03 11:54:54 +0300

    rewrite front to simple html

--- front/components/AppButton/index.jsx
+++
@@ -1,9 +0,0 @@
-import styles from './index.module.scss';
-
-export default function AppButton(props) {
-  return (
-    <button onClick={props.onClick} className={styles.button}>
-      <span>{props.text}</span>
-    </button>
-  );
-}
